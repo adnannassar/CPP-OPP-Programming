14db731d634838beb2d8e982a066bae751549f62
Adnan Nassar <mohamad-adnan.nassar@adesso.de>
2022-06-22 13:35:26 +0200

    added: von 17 to 21

--- Übungen/main.cpp
+++ Übungen/main.cpp
@@ -2,178 +2,251 @@
 #include <string>
 #include <list>
 #include <map>
+#include <vector>
 using namespace std;
 
-template <typename T, typename W>
-W sum(T a, W b) { return a + b; }
-
-struct rec
-{
-    int val;
-    char str[12];
-    int *a[2];
-};
-
-rec arr1[3];
-
-struct record
-{
-    char buf[8]; // char array
-    char *str;   // string array
-    int a[10];   // int array
-};
-record viele[6]; // record array -->
-                 // {record{buf{}, str{}, a{}},
-                 // record{buf{}, str{}, a{}},
-                 // record{buf{}, str{}, a{}}
-                 // record{buf{}, str{}, a{}}
-                 // record{buf{}, str{}, a{}}
-                 // record{buf{}, str{}, a{}}
-//}
-/*
-
-• p1 als Zeiger, der auf das vorletzte struct rec in arr zeigt.
-• p2 als unveränderliche Zeiger auf das letzte Element von arr. Verwenden Sie nur p1 und nicht arr.
-• r3 als unveränderliche Referenz auf das erste Element von arr.
-• r4 als Referenz auf das ersten Element des bufs des ersten Elements von arr.
-
-*/
-
-void fun(const int x) { cout << "a"; }
-void fun(const char x) { cout << "b"; }
-void fun(const int *x) { cout << "c"; }
-void fun(const char *x) { cout << "d"; }
-struct st
-{
-    // 1 placeholder
-    // char ch; // 1
-    short sh; // 2
-    int in;   // 4
-    // long l;
-};
-
-class Noisy
+class Time
 {
 public:
-    Noisy() { std::cout << "std" << std::endl; }
-    Noisy(const Noisy &) { std::cout << "con" << std::endl; }
-    Noisy(int i) { std::cout << "coi" << std::endl; }
-    Noisy &operator=(const Noisy &)
+    Time(int hour = 0, int minute = 0, int second = 0) : _hour(hour), _minute(minute), _second(second)
     {
-        std::cout << "ass" << std::endl;
-        return *this;
+        cout << "Created" << endl;
     }
+    ~Time() { cout << "Killed!" << endl; }
+    int hour() const { return _hour; }
+    int minute() const { return _minute; }
+    int second() const { return _second; }
+
+    bool add_hours(int hours)
+    {
+        if (_hour < 23)
+        {
+            _hour += hours;
+        }
+        return normalize();
+    }
+    bool add_minutes(int minutes)
+    {
+        _minute += minutes;
+        return normalize();
+    }
+    bool add_seconds(int seconds)
+    {
+        _second += seconds;
+        return normalize();
+    }
+    bool operator==(const Time &other) const
+    {
+        return _hour == other._hour && _minute == other._minute && _second == other._second;
+    }
+    void print()
+    {
+        cout << _hour << ":" << _minute << ":" << _second << endl;
+    }
+
+private:
+    bool normalize()
+    {
+        if (_second == 60)
+        {
+            _second = 0;
+            _minute++;
+            return true;
+        }
+        if (_minute == 60)
+        {
+            _minute = 0;
+            _hour++;
+            return true;
+        }
+        if (_hour == 23 && _minute == 59 && _second == 59)
+        {
+            _second = _minute = _hour = 0;
+            return true;
+        }
+
+        return false;
+    }
+    int _hour, _minute, _second;
 };
 
-class K
+Time operator++(Time &t, int)
+{
+    Time erg = t;
+    erg.add_seconds(1);
+    return erg;
+}
+istream &operator>>(istream &in, Time &t)
+{
+    int h, m, s;
+    cout << "hour: ";
+    in >> h;
+
+    cout << "minute: ";
+    in >> m;
+
+    cout << "secund: ";
+    in >> s;
+    t = Time(h, m, s);
+    return in;
+}
+class Uhr
 {
 private:
-    char a, b;
-    char *c;
+    Time t;
 
 public:
-    K() : K('a', 'b') {}
-    K(char a) : K(a, 'b') {}
-    K(char _a, char _b, const char *s = "nothing") : a(_a), b(_b)
+    Uhr(Time t) : t(t){};
+    virtual void tick()
     {
-        c = new char[strlen(s) + 1];
-        strcpy(c, s);
+        t.add_seconds(1);
     }
-    ~K()
+    Time getTime()
     {
-        delete[] c;
-    }
-
-    void show()
-    {
-        cout << a << " " << b << " " << c << endl;
-    }
-    char aa() const
-    {
-        return a;
-    }
-    char &aa()
-    {
-        return a;
-    }
-    char bb() const
-    {
-        return b;
-    }
-    char &bb()
-    {
-        return b;
-    }
-    char *cc() const
-    {
-        return c;
-    }
-
-    char &cc(int pos) const
-    {
-        return c[pos];
+        return t;
     }
 };
+
+class Wecker : public Uhr
+{
+private:
+    Time weckzeit;
+
+public:
+    Wecker(Time t, Time weckzeit) : Uhr(t), weckzeit(weckzeit) {}
+    void tick() override
+    {
+        Uhr::tick();
+        if (getTime() == weckzeit)
+        {
+            cout << "Alarm!!!" << endl;
+        }
+    }
+};
+
+class Fahrzeug
+{
+public:
+    virtual ~Fahrzeug() {}
+    virtual void fahren() = 0;
+};
+
+class Rennwagen : public Fahrzeug
+{
+public:
+    void fahren() override
+    {
+        cout << "Vroooooom." << endl;
+    }
+};
+
+class Traktor : public Fahrzeug
+{
+public:
+    void fahren() override
+    {
+        cout << "tuktuktuktuktuk." << endl;
+    }
+};
+
+enum Type
+{
+    APFEL,
+    BIRNE
+};
+
+class Obst
+{
+public:
+    virtual ~Obst(){}; // in dem Fall nicht unbedingt notwendig
+    virtual Type typeOf() = 0;
+};
+class Apfel : public Obst
+{
+public:
+    Type typeOf() override { return APFEL; }
+};
+class Birne : public Obst
+{
+public:
+    Type typeOf() override { return BIRNE; }
+};
+
+void testAufgabe17()
+{
+    Time t(11, 46, 20);
+    t.print();
+    // Time t2;
+    // cin >> t2;
+    // t2.print();
+    t = t++;
+    t.print();
+    Time t2;
+    cin >> t2;
+    if (t == t2)
+    {
+        cout << "Equals" << endl;
+    }
+    else
+    {
+        cout << "Not Equals" << endl;
+    }
+}
+void testAufgabe18()
+{
+    Wecker weaker(Time(12, 20, 30), Time(12, 20, 35));
+    for (int i = 1; i <= 5; i++)
+    {
+        weaker.getTime().print();
+        weaker.tick();
+    }
+}
+
+void testAufgabe19()
+{
+    list<shared_ptr<Fahrzeug>> list;
+    list.push_back(make_shared<Rennwagen>());
+    list.push_back(make_shared<Rennwagen>());
+    list.push_back(make_shared<Traktor>());
+    list.push_back(make_shared<Traktor>());
+
+    for (auto fahrzeug : list)
+    {
+        fahrzeug->fahren();
+    }
+}
+
+void obst_zaehlen(vector<Obst *> korb)
+{
+    int anzahlApfel = 0, anzahlBirne = 0;
+    for (Obst *o : korb)
+    {
+        if (o->typeOf() == APFEL)
+        {
+            anzahlApfel++;
+        }
+        if (o->typeOf() == BIRNE)
+        {
+            anzahlBirne++;
+        }
+    }
+    cout << "Es gibt " << anzahlApfel << " Äpfel und " << anzahlBirne << " Birnen" << endl;
+}
+
+void testAufgabe21(vector<Obst *> korb)
+{
+    obst_zaehlen(korb);
+}
 
 int main()
 
 {
-    K k1{'a', 'b', "abcdef"};
-    K k2{'c', 'd', "abcdef"};
-    const K k3{'e', 'f', "abcdef"};
-    cout << k1.aa() << k2.aa() << k3.aa() << endl; // ace
-    cout << k1.bb() << k2.bb() << k3.bb() << endl; // bdf
-    k1.aa() = k2.aa() = k3.aa();
-    k1.bb() = k2.bb() = k3.bb();
-    cout << k1.aa() << k2.aa() << k3.aa() << endl; // eee
-    cout << k1.bb() << k2.bb() << k3.bb() << endl; // fff
-    cout << k1.cc() << " " << k2.cc() << " " << k3.cc();
-    cout << endl;                                     // abcdef abcdef abcdef
-    cout << k1.cc(0) << k2.cc(1) << k3.cc(2) << endl; // abc
-    k1.cc(1) = k1.cc(2) = k2.cc(2) = k3.cc(3);
-    cout << k1.cc() << " " << k2.cc() << " " << k3.cc();
-    cout << endl; // adddef abddef abcdef
-    /*
-    st s;
-
-    cout << sizeof(s) << endl;
-
-
-     int a[17] = {0, 100, 200, 300, 400, 500, 120, 700, 8, 900, 10, 11, 12, 13, 14, 15, 16};
-     long b[42] = {0};
-     char c[20] = {1, 2, 3};
-     int i;
-     cout << &a[6] << endl;
-     cout << &a[2] << endl;
-     cout << (&a[6] - &a[2]) << endl;
-     cout << (&b[17] - &b[11]) << endl;
-     cout << (&c[3] - &c[1]) << endl
-
-     char s[] = "Hello";
-     //fun(s);
-     //fun(s[0]);
-     // fun(s[0] + 65536);
-     fun(65536);
-     cout << endl;
-
-
-     record *p1 = &viele[4];
-     record *p2 = p1 + 1;
-     const record &r3 = viele[0];
-     char &r4 = viele[0].buf[0];
-
-     auto p1 = &arr[1];
-     cout << "Size of p1:       " << sizeof(p1) << endl;
-     cout << "Size of &arr[2]:  " << sizeof(&arr[2]) << endl;
-     cout << "Adress of p1:       " << p1 << endl;
-     cout << "Adress of &arr[2]:  " << &arr[2] << endl;
-     cout << (&arr[2] - p1) << ", ";
-
-     auto p2 = &p1->str[4];
-     cout << "Size of p2:                   " << sizeof(p2) << endl;
-     cout << "Size of &arr[0].str[6]:         " << sizeof(&arr[0].str[6]) << endl;
-
-     cout << (p2 - &arr[0].str[6]) << endl;
-     */
+    vector<Obst *> korb = {
+        new Apfel(),
+        new Apfel(),
+        new Birne(),
+        new Birne(),
+        new Apfel()};
+    testAufgabe21(korb);
     return 1;
 }
